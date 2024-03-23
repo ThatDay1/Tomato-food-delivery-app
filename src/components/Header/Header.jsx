0291b81a26c79747475bebe8d612154b9a9c5e1a
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-03-24 00:40:52 +0300

    header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+          eligendi magni id exercitationem quis vero repellendus fuga illum
+          cumque, ex, quae cupiditate!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
